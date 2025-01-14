ec115b6d20f512b41786edfcbf626673a99d3522
Yury Lebedev <Yury.Lebedev@inovalon.global>
2025-01-14 04:28:00 -0500

    hchecks on orders

---
+++ OrderProcessor/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderProcessor.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Payments service is healthy");
+        }
+    }
+}
